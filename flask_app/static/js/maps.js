c8a6f014a05ef8a1940789ed0fd0a6b83c77730a
fjakuro <fjakuro@gmail.com>
2024-06-05 22:51:54 +0900

    JavaScriptファイルの整理

--- flask_app/static/js/maps.js
+++ flask_app/static/js/maps.js
@@ -1,30 +1,4 @@
-function makeMap() {
-    var map = L.map('map',
-    {
-        center: [36.105446527684215, 140.10174633069715], // 学生会館
-        zoom: 15,
-        zoomControl: false
-    });
-
-    // タイル選択
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(map);
-
-    // Zoomボタン
-    L.control.zoom({
-        position: 'bottomright'
-    }).addTo(map);
-
-    // 移動制限
-    const southWest = L.latLng(36.054055080143236, 140.0320789791267);
-    const northEast = L.latLng(36.14511141462569, 140.18986633469459);
-    const bounds = L.latLngBounds(southWest, northEast);
-    map.setMaxBounds(bounds);
-
-    return map;
-}
+import { makeMap } from "./map_utils.js"
 
 function renderData(map) {
     var dataUrl = "/data"; // jsonデータのあるエンドポイント
@@ -94,3 +68,4 @@ $(function() {
         };
     });
 });
+
